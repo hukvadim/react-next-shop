fc4482390903e74cbf8c1032c88e42b8e1e29da5
Vadim <hukvadim@gmail.com>
2023-12-11 20:41:17 +0200

    11

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	
+	images: {
+		loader: 'imgix',
+		path: 'https://hukvadim.github.io/react-next-shop/',
+	  },
 }
 
 module.exports = nextConfig
