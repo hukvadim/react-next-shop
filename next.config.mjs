6418fc99154696e1b884fac9154367995496edba
Vadim <hukvadim@gmail.com>
2024-02-10 18:06:23 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-
-    // Add basePath
-    basePath: "/react-next-shop",
+    basePath: ""
 };
 
 export default nextConfig;
