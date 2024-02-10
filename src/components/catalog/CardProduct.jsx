777165b408210b13ebca99fa83304f444f8cff0c
Vadim <hukvadim@gmail.com>
2024-02-10 19:17:55 +0200

    Update CardProduct.jsx

--- src/components/catalog/CardProduct.jsx
+++ src/components/catalog/CardProduct.jsx
@@ -4,7 +4,7 @@ const CardProduct = () => {
 	return (
 		<div className="card-product">
 			<div className="card-product__img-hold">
-				<Image src="/img/catalog/1.png" alt="" className="card-product__img" width={264} height={264} priority={false}/>
+				<Image src="/img/catalog/1.png" alt="" className="card-product__img" width={264} height={264} priority={true}/>
 			</div>
 			<div className="card-product__text-hold">
 				<a href="#" className="card-product__title-link">Планшет Lenovo Tab M10 Plus 4/128 Grey</a>
